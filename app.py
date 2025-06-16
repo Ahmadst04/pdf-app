9ea373be9933d9ab368bdb8fd03c04eb04a0fd5e
Ahmadst04 <haqqanyaqin@gmail.com>
2025-06-16 14:44:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import io
 from collections import defaultdict
 
 st.set_page_config(page_title="PDF Table Extractor", layout="wide")
-st.title("📄 PDF to Excel Converter")
+st.title("📄 PDF to Excel Converter")2
 
 uploaded_file = st.file_uploader("Upload a PDF", type="pdf")
 buffer = None
